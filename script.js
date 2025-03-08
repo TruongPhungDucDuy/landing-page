90130c07981bd079d77ba48d416cd48debfa68ff
TruongPhungDucDuy <145097787+TruongPhungDucDuy@users.noreply.github.com>
2025-03-09 04:46:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("orderForm").addEventListener("submit", function(event) 
         return;
     }
 
-    fetch("https://docs.google.com/spreadsheets/d/16esT-z7EOKQJAfWNy3fFaOUIZQu1ejz-mxPuGqebiuw/edit?gid=0#gid=0", {
+    fetch("https://docs.google.com/spreadsheets/d/16esT-z7EOKQJAfWNy3fFaOUIZQu1ejz-mxPuGqebiuw/edit?usp=sharing", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, phone, address })
